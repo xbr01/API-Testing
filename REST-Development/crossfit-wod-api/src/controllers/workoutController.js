5059331d5d046d3f4c58f2013929988dc3bd5010
xbr01 <abhiram26333@gmail.com>
2024-08-20 12:24:31 +0530

    have to setup db

---
+++ REST-Development/crossfit-wod-api/src/controllers/workoutController.js
@@ -0,0 +1,39 @@
+const workoutService = require("../services/workoutService");
+
+const getAllWorkouts = (req, res) => {
+  // *** ADD ***
+  const allWorkouts = workoutService.getAllWorkouts();
+  res.send("Get all workouts");
+};
+
+const getOneWorkout = (req, res) => {
+  // *** ADD ***
+  const workout = workoutService.getOneWorkout();
+  res.send("Get an existing workout");
+};
+
+const createNewWorkout = (req, res) => {
+  // *** ADD ***
+  const createdWorkout = workoutService.createNewWorkout();
+  res.send("Create a new workout");
+};
+
+const updateOneWorkout = (req, res) => {
+  // *** ADD ***
+  const updatedWorkout = workoutService.updateOneWorkout();
+  res.send("Update an existing workout");
+};
+
+const deleteOneWorkout = (req, res) => {
+  // *** ADD ***
+  workoutService.deleteOneWorkout();
+  res.send("Delete an existing workout");
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
